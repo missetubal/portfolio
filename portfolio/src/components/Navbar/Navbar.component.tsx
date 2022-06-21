119248042385c70236f99e40d2270272e902f2fc
Michelle Setúbal <missetubal@outlook.com>
2022-06-20 22:01:51 -0300

    feat: navbar component

--- portfolio/src/components/Navbar/Navbar.component.tsx
+++ portfolio/src/components/Navbar/Navbar.component.tsx
@@ -1,9 +1,29 @@
-import React from 'react';
-
-// import { Container } from './styles';
+import React, { useState } from 'react';
+import Moon from '../../assets/MoonBlack';
+import { Image, NavContainer, NavLeft, NavOptions, NavRight, Pages, SwitchColor, TextLeft } from './Navbar.styles';
 
 const Navbar: React.FC = () => {
-  return <p>rt</p>;
+  const [isDark, setIsDark] = useState(false)
+  return (
+    <NavContainer>
+      <NavLeft>
+        <Image src='../../../public/assets/face.png'/>
+        <TextLeft>Michelle Setúbal</TextLeft>
+      </NavLeft>
+      <NavRight>
+        <NavOptions>
+          <Pages href='/'>Inicio</Pages>
+          <Pages href='/about'>Sobre</Pages>
+          <Pages href=''>Experiências</Pages>
+          <Pages href='/projects'>Projetos</Pages>
+          <Pages href='/contact'>Contato</Pages>
+        </NavOptions>
+        <SwitchColor onClick={()=>setIsDark(!isDark)}>
+          <Moon />
+        </SwitchColor>
+      </NavRight>
+    </NavContainer>
+  );
 }
 
 export default Navbar;
