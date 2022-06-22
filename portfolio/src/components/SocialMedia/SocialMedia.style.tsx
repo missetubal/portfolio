b401732414ad744e83f648afa2301bc77a0b41ca
Michelle Setúbal <missetubal@outlook.com>
2022-06-21 21:34:39 -0300

    chore: social media component

---
+++ portfolio/src/components/SocialMedia/SocialMedia.style.tsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const MediaContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    margin-left: 50px;
+    align-items: baseline;
+`
+export const Medias = styled.a`
+    margin: 5px 0px;
+`
