4133b9bfc1cccc3a4048e71c633fb730f3091d5c
Michelle Setúbal <missetubal@outlook.com>
2022-07-13 14:01:02 -0300

    chore: update App

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
+import MenuMobile from './components/MenuMobile/MenuMobile.component';
 import Navbar from './components/Navbar/Navbar.component';
+import AnimatedRoute from './routes/AnimatedRoutes';
+import { GlobalStyle } from './styles/general.styles';
 
 function App() {
   return (
-    <div className="App">
+    // <GlobalStyle>
+    <>
       <Navbar/>
-    </div>
+      <MenuMobile/>
+      <AnimatedRoute/>
+    </>
+     
+    // </GlobalStyle>
+
   );
 }
 
